1c211788e55ff3f22a522236e172074c5799e65f
Rakib <rakibs142@gmail.com>
2026-06-27 00:40:22 +0530

    Project Initialization

---
+++ EulerityTakeHomeTest/EulerityTakeHomeTest/EulerityTakeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EulerityTakeHomeTestApp.swift
+//  EulerityTakeHomeTest
+//
+//  Created by Rakib on 27/06/26.
+//
+
+import SwiftUI
+
+@main
+struct EulerityTakeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
